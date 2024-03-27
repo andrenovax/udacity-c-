ebebea3c21c672c32034615ed298025d5e4d605a
Andrey Novak <andrenovax@gmail.com>
2024-03-27 16:37:38 +0100

    ref gridsearch

--- src/gridsearch.cpp
+++ src/gridsearch.cpp
@@ -2,68 +2,32 @@
 
 #include <algorithm>
 #include <string>
-#include <vector>
 
-using std::abs;
 using std::sort;
-using std::vector;
+using std::array;
+using std::optional;
 
-/**
- * Compare the F values of two cells.
- */
-bool GridSearch::Compare(const Node &a, const Node &b) {
-  return (a.g + a.h) > (b.g + b.h);
-}
-
-/**
- * Sort the two-dimensional vector of ints in descending order
- */
-void GridSearch::CellSort(Nodes *v)  {
-  sort(v->begin(), v->end(), Compare);
-}
-
-// Calculate the manhattan distance
-int GridSearch::Heuristic(int x1, int y1, int x2, int y2) {
-  return abs(x2 - x1) + abs(y2 - y1);
-}
-
-/**
- * Expand current nodes neighbors and add them to the open list.
- */
-void GridSearch::ExpandNeighbors(Node* current, int goal[2], Nodes &openlist) {
-  // Loop through current node's potential neighbors.
-  for (int i = 0; i < 4; i++) {
-    int x2 = current->x + NODE_NEIGHBOUR_DELTA[i][0];
-    int y2 = current->y + NODE_NEIGHBOUR_DELTA[i][1];
-
-    if (x2 == _grid.Width()) {
-      x2 = 0;
-    } else if (x2 < 0) {
-      x2 = _grid.Width() - 1;
-    }
-
-    if (y2 == _grid.Height()) {
-      y2 = 0;
-    } else if (y2 < 0) {
-      y2 = _grid.Height() - 1;
-    }
-
-    if (_grid.IsEmpty(x2, y2)) {
-      int g2 = current->g + 1;
-      int h2 = Heuristic(x2, y2, goal[0], goal[1]);
-      openlist.push_back({x2, y2, g2, h2, current});
-      _grid.Close(x2, y2);
+void GridSearch::ExpandNeighbors(Grid::Node *current, Grid::Node *end_node, Nodes &openlist) {
+  auto neighbours = _grid.GetNeighbourNodes(*current);
+  for (auto &neighbour : neighbours) {
+    if (neighbour->IsEmpty()) {
+      neighbour->g = current->g + 1;
+      neighbour->h = neighbour->Distance(end_node);
+      neighbour->parent = current;
+      neighbour->state = Grid::Node::State::kPath;
+      openlist.push_back(neighbour);
     }
   }
 }
 
-std::vector<GridSearch::Node> GridSearch::MakePath(Node &current_node, Node &start_node) {
+GridSearch::Nodes GridSearch::MakePath(Grid::Node *current_node, Grid::Node *start_node) {
   Nodes path_found{current_node};
 
-  Node *current = &current_node;
-  while (current != &start_node) {
-    auto parent = current_node.parent;
-    path_found.push_back(*parent);
+  Grid::Node *current = current_node;
+
+  while (current != start_node) {
+    auto parent = current_node->parent;
+    path_found.push_back(parent);
     current = parent;
   }
 
@@ -72,37 +36,35 @@ std::vector<GridSearch::Node> GridSearch::MakePath(Node &current_node, Node &sta
   return path_found;
 }
 
-std::optional<GridSearch::Nodes> GridSearch::AStarSearch(std::array<int, 2> init,
-                                                         int goal[2]) {
-  int x = init[0];
-  int y = init[1];
-  int g = 0;
-  int h = Heuristic(x, y, goal[0], goal[1]);
+optional<GridSearch::Nodes> GridSearch::AStarSearch(XY_Coords init, XY_Coords goal) {
+  auto end_node{_grid.GetNode(goal[0], goal[1])};
+  auto start_node{_grid.GetNode(init[0], init[1])};
+  start_node->g = 0;
+  start_node->h = start_node->Distance(end_node);
 
-  Node start_node{x, y, g, h};
   Nodes open{start_node};
 
   while (open.size() > 0) {
     // Get the next node
-    CellSort(&open);
-    auto current = open.back();
+    sort(open.begin(), open.end());
+    auto next_node = open.back();
     open.pop_back();
 
-    if (current.x == goal[0] && current.y == goal[1]) {
-      return MakePath(current, start_node);
+    if (next_node == end_node) {
+      return MakePath(next_node, start_node);
     }
 
-    ExpandNeighbors(&current, goal, open);
+    ExpandNeighbors(next_node, end_node, open);
   }
 }
 
-std::optional<std::array<int, 2>> GridSearch::FindNext(std::array<int, 2> init, int goal[2]) {
+optional<GridSearch::XY_Coords> GridSearch::FindNext(XY_Coords init, XY_Coords goal) {
   auto path = AStarSearch(init, goal);
   if (path.has_value()) {
     auto path_value = path.value();
     if (path_value.size() > 1) {
       auto next_node = path_value[1];
-      return std::optional<std::array<int, 2>>{{next_node.x, next_node.y}};
+      return optional<XY_Coords>{{next_node->x, next_node->y}};
     }
   }
 }
--- src/gridsearch.h
+++ src/gridsearch.h
@@ -3,43 +3,31 @@
 
 #include <vector>
 #include <array>
+#include <optional>
 
 #include "grid.h"
 
-using std::abs;
-using std::sort;
 using std::vector;
 using std::array;
+using std::optional;
 
 class GridSearch {
  public:
-  struct Node {
-    int x, y, g, h;
-    Node* parent;
-
-    Node(int x, int y, int g, int h, Node *parent = nullptr)
-        : x{x}, y{y}, g{g}, h{h}, parent{parent} {}
-  };
-
-  typedef vector<Node> Nodes;
+  typedef vector<Grid::Node*> Nodes;
+  typedef array<int, 2> XY_Coords;
 
   // copy grid, so we can modify grid here without affecting the main one
   GridSearch(Grid grid) : _grid{grid} {};
 
-  std::optional<std::array<int, 2>> FindNext(std::array<int, 2> init, int goal[2]);
-  std::optional<Nodes> AStarSearch(std::array<int, 2> init, int goal[2]);
+  optional<XY_Coords> FindNext(XY_Coords init, XY_Coords goal);
+  optional<Nodes> AStarSearch(XY_Coords init, XY_Coords goal);
 
  private:
   Grid _grid;
 
-  static constexpr int NODE_NEIGHBOUR_DELTA[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-  static bool Compare(const Node &a, const Node &b);
-  static void CellSort(Nodes *v);
-  static int Heuristic(int x1, int y1, int x2, int y2);
+  static Nodes MakePath(Grid::Node *current_node, Grid::Node *start_node);
 
-  static vector<Node> MakePath(Node &current_node, Node &start_node);
-
-  void ExpandNeighbors(Node* current, int goal[2], Nodes &openlist);
+  void ExpandNeighbors(Grid::Node *current,  Grid::Node *end_node, Nodes &openlist);
 };
 
 #endif
